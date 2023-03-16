b32550fbbb04f14a5f7ea956ab4ef7cf82bb6c23
Joel Ortiz Rivera <joelrivera.oa@gmail.com>
2023-03-15 20:21:39 -0600

    Version de testeo XDXDXD

--- src/ahorcado/Ahorcado.java
+++ src/ahorcado/Ahorcado.java
@@ -4,17 +4,51 @@
  */
 package ahorcado;
 
-/**
- *
- * @author choungion
- */
+import java.util.*;
+
 public class Ahorcado {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
+        Scanner leer = new Scanner(System.in);
+        String palabra = "automovil";
+
+        Queue<Character> letrasDePalabraAdivinar = new LinkedList<>();
+        letrasDePalabraAdivinar.add(palabra.charAt(0));
+
+        Stack<Integer> partesDeMuneco = new Stack<>();
+        partesDeMuneco.push(1);
+        partesDeMuneco.push(2);
+        partesDeMuneco.push(3);
+        partesDeMuneco.push(4);
+        partesDeMuneco.push(5);
+
+        while (true){
+            System.out.println("Palabra: "+letrasDePalabraAdivinar);
+
+            if(letrasDePalabraAdivinar.size() == palabra.length()){
+                System.out.println("Has ganado el juego");
+                break;
+            }
+
+            if (partesDeMuneco.isEmpty()){
+                System.out.println("Has perdido el juego");
+                break;
+            }
+
+            System.out.print("Escriba una letra: ");
+            //Se captura en string, luego se transforma en char
+            String letra = leer.nextLine();
+            if (letra.length() == 1){
+                char letraEnChar = letra.charAt(0);
+                if (letraEnChar == palabra.charAt(letrasDePalabraAdivinar.size())){
+                    letrasDePalabraAdivinar.add(letraEnChar);
+                } else{
+                    partesDeMuneco.pop();
+                    System.out.println("Se ha cortado una parte del muñeco, sobran "+partesDeMuneco.size()+" partes");
+                }
+            }
+        }
+
     }
-    
+
 }
